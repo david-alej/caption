1b0b880684beea11ca376afde91e921a10f8e9bf
david-alej <david12777@outlook.com>
2023-10-11 15:39:45 -0600

    redid defining the model relationships

---
+++ database/seeders/20231011211537-User.js
@@ -0,0 +1,31 @@
+"use strict"
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    queryInterface.bulkInsert("User", [
+      {
+        username: "janedoe",
+        password: "janedoe",
+      },
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+}
